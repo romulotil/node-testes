3c034326070a34a4ce93e9000be1235f0b37f0f8
Romulo Silva <Romulo Silva>
2017-01-04 00:18:22 -0200

    aulas 3 e 4

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,6 +1,22 @@
 module.exports = function (app) {
-        app.get('/produtos', (request, response) => {
-        response.render('produtos/lista');
+    app.get('/produtos', (request, response) => {
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '200459',
+            database: 'cdc_nodejs'
+        });
+        
+        connection.query('select * from livros', function (erro, resultado) {
+            console.log(resultado);
+
+            response.render('produtos/lista', {
+                lista: resultado
+            });
+        });
+        
+        connection.end();
     });
     return app;
 }
